a7b5ebf90858802b24f0d3f1f12db9daf5477035
Bar Hochman <11165655+jochman@users.noreply.github.com>
2018-09-18 12:01:12 +0300

    Added password
    *SSID: Roboto
    *Password: RobotoPass
    
    *Increased readabilty

--- NodeMCU/NodeMCU.ino
+++ NodeMCU/NodeMCU.ino
@@ -1,6 +1,7 @@
 /* include library */
 #include <ESP8266WiFi.h>
 #define SSID "Roboto"
+#define PASSWORD "RobotoPass"
 
 /* define port */
 WiFiClient client;
@@ -8,10 +9,10 @@ WiFiServer server(666);
 
 /* WIFI settings */
 IPAddress subnet(255, 255, 255, 0);
-IPAddress apIP(10,10,10,1); 
+IPAddress apIP(10, 10, 10, 1);
 
 /* data received from application */
-String  data =""; 
+String  data = "";
 
 /* define L298N or L293D motor control pins */
 const int leftMotorForward = 2;     /* GPIO2(D4) -> IN3   */
@@ -28,24 +29,24 @@ void setup()
 {
   /* initialize motor control pins as output */
   pinMode(leftMotorForward, OUTPUT);
-  pinMode(rightMotorForward, OUTPUT); 
-  pinMode(leftMotorBackward, OUTPUT);  
+  pinMode(rightMotorForward, OUTPUT);
+  pinMode(leftMotorBackward, OUTPUT);
   pinMode(rightMotorBackward, OUTPUT);
 
   /* initialize motor enable pins as output */
-  pinMode(leftMotorENB, OUTPUT); 
+  pinMode(leftMotorENB, OUTPUT);
   pinMode(rightMotorENB, OUTPUT);
 
   /* start server communication */
   Serial.begin(9600);
   Serial.println("booting up");
   Serial.println("Connecting to WIFI");
-  
+
   /* Setting WiFi */
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, subnet);
-  WiFi.softAP(SSID);
-  
+  WiFi.softAP(SSID, PASSWORD);
+
   Serial.println("");
   Serial.println("WiFi connected");
 
@@ -61,94 +62,94 @@ void setup()
 
 void loop()
 {
-    /* If the server available, run the "checkClient" function */  
-    client = server.available();
-    if (!client) {
-      return; 
-    }
-    
-    data = client.readStringUntil('\r');
+  /* If the server available, run the "checkClient" function */
+  client = server.available();
+  if (!client) {
+    return;
+  }
 
-/************************ Run function according to incoming data from application *************************/
+  data = client.readStringUntil('\r');
 
-    /* If the incoming data is "forward", run the "MotorForward" function */
-    if (data == "forward") {
-      Serial.println("moving forward");
-      MotorForward();
-    }
-    /* If the incoming data is "backward", run the "MotorBackward" function */
-    else if(data == "backward") {
-      Serial.println("moving backwards");
-      MotorBackward();
-    }
-    /* If the incoming data is "left", run the "TurnLeft" function */
-    else if (data == "left"){
-      Serial.println("moving left");
-      TurnLeft();
-    }
-    /* If the incoming data is "right", run the "TurnRight" function */
-    else if (data == "right"){
-      Serial.println("moving right");
-      TurnRight();
-    }
-    /* If the incoming data is "stop", run the "MotorStop" function */
-    else if (data == "stop"){
-      Serial.println("stopping motor");
-      MotorStop();
-    }
-} 
+  /************************ Run function according to incoming data from application *************************/
+
+  /* If the incoming data is "forward", run the "MotorForward" function */
+  if (data == "forward") {
+    Serial.println("moving forward");
+    MotorForward();
+  }
+  /* If the incoming data is "backward", run the "MotorBackward" function */
+  else if (data == "backward") {
+    Serial.println("moving backwards");
+    MotorBackward();
+  }
+  /* If the incoming data is "left", run the "TurnLeft" function */
+  else if (data == "left") {
+    Serial.println("moving left");
+    TurnLeft();
+  }
+  /* If the incoming data is "right", run the "TurnRight" function */
+  else if (data == "right") {
+    Serial.println("moving right");
+    TurnRight();
+  }
+  /* If the incoming data is "stop", run the "MotorStop" function */
+  else if (data == "stop") {
+    Serial.println("stopping motor");
+    MotorStop();
+  }
+}
 
 /********************************************* FORWARD *****************************************************/
-void MotorForward(void)   
+void MotorForward(void)
 {
-  digitalWrite(leftMotorENB,HIGH);
-  digitalWrite(rightMotorENB,HIGH);
-  digitalWrite(leftMotorForward,HIGH);
-  digitalWrite(rightMotorForward,HIGH);
-  digitalWrite(leftMotorBackward,LOW);
-  digitalWrite(rightMotorBackward,LOW);
+  digitalWrite(leftMotorENB, HIGH);
+  digitalWrite(rightMotorENB, HIGH);
+  digitalWrite(leftMotorForward, HIGH);
+  digitalWrite(rightMotorForward, HIGH);
+  digitalWrite(leftMotorBackward, LOW);
+  digitalWrite(rightMotorBackward, LOW);
 }
 
 /********************************************* BACKWARD *****************************************************/
-void MotorBackward(void)   
+void MotorBackward(void)
 {
-  digitalWrite(leftMotorENB,HIGH);
-  digitalWrite(rightMotorENB,HIGH);
-  digitalWrite(leftMotorBackward,HIGH);
-  digitalWrite(rightMotorBackward,HIGH);
-  digitalWrite(leftMotorForward,LOW);
-  digitalWrite(rightMotorForward,LOW);
+  digitalWrite(leftMotorENB, HIGH);
+  digitalWrite(rightMotorENB, HIGH);
+  digitalWrite(leftMotorBackward, HIGH);
+  digitalWrite(rightMotorBackward, HIGH);
+  digitalWrite(leftMotorForward, LOW);
+  digitalWrite(rightMotorForward, LOW);
 }
 
 /********************************************* TURN LEFT *****************************************************/
-void TurnLeft(void)   
+void TurnLeft(void)
 {
-  digitalWrite(leftMotorENB,HIGH);
-  digitalWrite(rightMotorENB,HIGH); 
-  digitalWrite(leftMotorForward,LOW);
-  digitalWrite(rightMotorForward,HIGH);
-  digitalWrite(rightMotorBackward,LOW);
-  digitalWrite(leftMotorBackward,HIGH);  
+  digitalWrite(leftMotorENB, HIGH);
+  digitalWrite(rightMotorENB, HIGH);
+  digitalWrite(leftMotorForward, LOW);
+  digitalWrite(rightMotorForward, HIGH);
+  digitalWrite(rightMotorBackward, LOW);
+  digitalWrite(leftMotorBackward, HIGH);
 }
 
 /********************************************* TURN RIGHT *****************************************************/
-void TurnRight(void)   
+void TurnRight(void)
 {
-  digitalWrite(leftMotorENB,HIGH);
-  digitalWrite(rightMotorENB,HIGH);
-  digitalWrite(leftMotorForward,HIGH);
-  digitalWrite(rightMotorForward,LOW);
-  digitalWrite(rightMotorBackward,HIGH);
-  digitalWrite(leftMotorBackward,LOW);
+  digitalWrite(leftMotorENB, HIGH);
+  digitalWrite(rightMotorENB, HIGH);
+  digitalWrite(leftMotorForward, HIGH);
+  digitalWrite(rightMotorForward, LOW);
+  digitalWrite(rightMotorBackward, HIGH);
+  digitalWrite(leftMotorBackward, LOW);
 }
 
 /********************************************* STOP *****************************************************/
-void MotorStop(void)   
+void MotorStop(void)
 {
-  digitalWrite(leftMotorENB,LOW);
-  digitalWrite(rightMotorENB,LOW);
-  digitalWrite(leftMotorForward,LOW);
-  digitalWrite(leftMotorBackward,LOW);
-  digitalWrite(rightMotorForward,LOW);
-  digitalWrite(rightMotorBackward,LOW);
+  digitalWrite(leftMotorENB, LOW);
+  digitalWrite(rightMotorENB, LOW);
+  digitalWrite(leftMotorForward, LOW);
+  digitalWrite(leftMotorBackward, LOW);
+  digitalWrite(rightMotorForward, LOW);
+  digitalWrite(rightMotorBackward, LOW);
 }
