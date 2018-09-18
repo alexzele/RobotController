0d62b42e4f2e510fb282b661c4db2305f972aa15
Bar Hochman <11165655+jochman@users.noreply.github.com>
2018-09-18 12:02:50 +0300

    Added user ip
    the user can pick up his own ip
    increased readability

--- Android/RoboContr/app/src/main/java/com/example/jochm/robocontr/MainActivity.java
+++ Android/RoboContr/app/src/main/java/com/example/jochm/robocontr/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedInputStream;
@@ -20,24 +21,20 @@ import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView t;
+    static String ip = "10.10.10.1";
+    TextView logTextView;
+    EditText ipEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        t = findViewById(R.id.logoMain);
-        if (isNetworkAvailable()) {
-            t.setText("Press any button to start playing around");
-        } else {
-            t.setText("NO NETWORK DETECTED");
-            ((Button) findViewById(R.id.backwards)).setVisibility(View.INVISIBLE);
-            ((Button) findViewById(R.id.forward)).setVisibility(View.INVISIBLE);
-            ((Button) findViewById(R.id.button_stop)).setVisibility(View.INVISIBLE);
-            ((Button) findViewById(R.id.left)).setVisibility(View.INVISIBLE);
-            ((Button) findViewById(R.id.right)).setVisibility(View.INVISIBLE);
-        }
+        /* Setting text views vars */
+        logTextView = findViewById(R.id.logoMain);
+        logTextView.setText("Press any button to start playing around");
+        ipEditText = findViewById(R.id.ipAdd);
+        ipEditText.setText(ip);
 
 
     }
@@ -45,13 +42,13 @@ public class MainActivity extends AppCompatActivity {
     /*--------------Message picker-------------*/
     public void picker(View view){
         String str = ((Button) view).getText().toString().toLowerCase();
+        ip = ipEditText.getText().toString();
         sendMessage(str);
     }
 
 
     /*--------------Server Code----------------*/
     private void sendMessage(final String msg) {
-        final String ip = "192.168.1.177";
         final Integer port = 666;
 
 
@@ -60,40 +57,28 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
 
                 try {
-                    //Replace below IP with the IP of that device in which server socket open.
-                    //If you change port then change the port number in the server side code also.
+                    /* Opening socket */
                     Socket s = new Socket(ip, port);
-
                     DataOutputStream out = new DataOutputStream(s.getOutputStream());
-                    out.flush();
 
+                    /* Cleaning stream and sending message*/
+                    out.flush();
                     out.writeBytes(msg + "\r");
 
+                    /* Close socket and stream */
                     out.close();
                     s.close();
 
-                    //t.setText("message sent");
-                    System.out.println("message sent, the server is " + s.isConnected());
+                    System.out.println("message \"" + msg + "\" sent, the server is " + s.isConnected());
                 } catch (IOException e) {
                     e.printStackTrace();
-                    //t.setText("failed to send message");
                 }
             }
         });
 
         thread.start();
         Log.d("BAR", "thread started");
-        while(thread.isAlive()){
-            //do nothing
-        }
-        Log.d("BAR", "thread finished");
-        t.setText("Moving " + msg);
-    }
 
-    private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        logTextView.setText("Moving " + msg);
     }
 }
